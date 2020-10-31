0dc433fd4f07aff8d0522cffe0366c1c27bee20f
Michael Duerinckx <mich@michd.me>
2020-10-31 21:57:40 +0000

    Add more LED-related tests

--- Src/ledtests.c
+++ Src/ledtests.c
@@ -7,6 +7,12 @@ static uint8_t input_data[64];
 
 static void test_hardcoded_LEDs();
 static void test_solid_color_LEDs();
+static void test_segment_solid_color_LEDs();
+static void test_commit_leds();
+
+static uint8_t g_offset = 0;
+static uint8_t r_offset = 1;
+static uint8_t b_offset = 2;
 
 static inline void handle_received_command(Commands cmd) {
     switch (cmd) {
@@ -17,6 +23,14 @@ static inline void handle_received_command(Commands cmd) {
         case Command_Test_Solid_Color_LEDs:
             uart_expect_data(input_data, 3);
             break;
+
+        case Command_Test_Segment_Solid_Color_LEDs:
+            uart_expect_data(input_data, 4);
+            break;
+
+        case Command_Test_Commit_LEDs:
+            test_commit_leds();
+            break;
     }
 }
 
@@ -24,6 +38,10 @@ static inline void handle_received_data(Commands cmd) {
     switch (cmd) {
         case Command_Test_Solid_Color_LEDs:
             test_solid_color_LEDs();
+            break;
+
+        case Command_Test_Segment_Solid_Color_LEDs:
+            test_segment_solid_color_LEDs();
             break;
     }
 }
@@ -42,9 +60,6 @@ void ledtests_handle_tests() {
 
 static void test_hardcoded_LEDs() {
     uint8_t segment_buffer[64];
-    uint8_t g_offset = 0;
-    uint8_t r_offset = 1;
-    uint8_t b_offset = 2;
 
     for (uint8_t led = 0; led < 21; led++) {
         segment_buffer[1 + led * 3 + r_offset] = led % 3 == 0 ? 0x20 : 0x00;
@@ -62,13 +77,10 @@ static void test_hardcoded_LEDs() {
 
 static void test_solid_color_LEDs() {
     uint8_t segment_buffer[64];
-    uint8_t g_offset = 0;
-    uint8_t r_offset = 1;
-    uint8_t b_offset = 2;
 
     for (uint8_t led = 0; led < 21; led++) {
-        segment_buffer[1 + led * 3 + g_offset] = input_data[1];
         segment_buffer[1 + led * 3 + r_offset] = input_data[0];
+        segment_buffer[1 + led * 3 + g_offset] = input_data[1];
         segment_buffer[1 + led * 3 + b_offset] = input_data[2];
     } 
 
@@ -77,5 +89,28 @@ static void test_solid_color_LEDs() {
         led_prepare_input(segment_buffer);
     }
 
+    // Remove the actual send here?
+    led_send_buffer();
+}
+
+static void test_segment_solid_color_LEDs() {
+    uint8_t segment_buffer[64];
+
+    uint8_t segment = input_data[0];
+    uint8_t red = input_data[1];
+    uint8_t green = input_data[2];
+    uint8_t blue = input_data[3];
+
+    for (uint8_t led = 0; led < 21; led++) {
+        segment_buffer[1 + led * 3 + r_offset] = red;
+        segment_buffer[1 + led * 3 + g_offset] = green;
+        segment_buffer[1 + led * 3 + b_offset] = blue;
+    }
+
+    segment_buffer[0] = (segment << 4) & 0x30;
+    led_prepare_input(segment_buffer);
+}
+
+static void test_commit_leds() {
     led_send_buffer();
 }
